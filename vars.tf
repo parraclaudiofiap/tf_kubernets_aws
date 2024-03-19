dc2e3c9b55614844559972a91bdd2511be3d276b
parraclaudio <parra.claudio@hotmail.com>
2024-03-19 12:36:14 -0300

    ajustes

---
+++ vars.tf
@@ -0,0 +1,11 @@
+variable "aws_region_default" {
+  default = "us-east-1"
+}
+
+variable "project_name" {
+   default     = "app-techchallenge"
+ }
+ 
+ variable "aws_account_id" {
+   default     = "341161836869"
+ }
